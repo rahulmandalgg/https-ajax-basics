23cf3f08c27978d34c30c3980ab80844b03d2976
Rahul <lachrymoseeee@gmail.com>
2022-05-31 16:56:29 +0530

    ajax basics

--- js/script.js
+++ js/script.js
@@ -1,43 +1,26 @@
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.txt", 
+            function (request) {
+              var name = request.responseText;
 
-// console.log(document.getElementById("title"));
+              document.querySelector("#content")
+                .innerHTML = "<h2>Hello " + name + "!</h2>";
+            });
 
-document.addEventListener("DOMContentLoaded", 
-	function (event) {
+        
+      });
+  }
+);
 
-		function sayHello(event) {
-			//console.log(event);
-			this.textContent = "Said it!";
-			var name = document.getElementById("name").value;
-			var msg = "<h2>hello " + name + "!</h2>";		
-			
-			// document.getElementById("content").textContent = msg;
-
-			document.getElementById("content").innerHTML = msg;
-
-			if (name === "student") {
-				var title = document.querySelector("#title").textContent;
-				title += " & Lovin' it!";
-
-				var title = document.querySelector("h1").textContent = title;
-
-				}
-
-			}
-
-			document.querySelector("button").onclick = sayHello;
-
-			document.querySelector("body").addEventListener("mousemove", function (event) {
-				if (event.shiftKey === true) {
-					console.log("x: " + event.clientX);
-					console.log("y: " + event.clientY);
-					}
-				}
-			);
-		}
-	);
-
-
-// document.querySelector("button").addEventListener("click",sayHello);
 
 
 
